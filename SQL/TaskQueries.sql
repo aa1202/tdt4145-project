c3e6588bfb3860e8df05075a52fe58684f130631
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-03-11 21:40:52 +0100

    Progress on task c.

--- SQL/TaskQueries.sql
+++ SQL/TaskQueries.sql
@@ -20,3 +20,10 @@ SELECT * FROM Selskap S
 		ON H.MediaID = M.MediaID
 	INNER JOIN Sjanger SJ 
 		ON SJ.SjangerID = H.SjangerID;
+        
+# C Bedre        
+SELECT Sjanger.Sjanger, Selskap.url,  count(HarSjanger.SjangerID) as AntallFilmer
+                    FROM HarSjanger, EierAvMedia, Sjanger, Selskap
+                    WHERE EierAvMedia.SelskapID = Selskap.SelskapID
+                    AND Sjanger.SjangerID = HarSjanger.SjangerID
+                    AND HarSjanger.MediaID = EierAvMedia.MediaID GROUP BY Sjanger.Sjanger, Selskap.url;
