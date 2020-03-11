33b6d15ce99b722a0c70cb99165da7af6f220b77
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-03-11 21:23:21 +0100

    Add file.

---
+++ SQL/TaskQueries.sql
@@ -0,0 +1,22 @@
+USE FilmDB;
+# a
+SELECT rolle FROM SkuespillerI as S INNER JOIN Person as P ON S.fnr = P.fnr WHERE Navn="Ola Halvorsen";
+
+# b
+SELECT M.* FROM Person P 
+	INNER JOIN SkuespillerI S 
+		ON P.fnr = S.fnr 
+	INNER JOIN Media M 
+		ON S.MediaID = M.MediaID 
+	WHERE P.navn="Ola Halvorsen";
+
+# c
+SELECT * FROM Selskap S
+	INNER JOIN EierAvMedia E 
+		ON E.SelskapID = S.SelskapID
+	INNER JOIN Media M 
+		ON M.MediaID = E.MediaID
+	INNER JOIN HarSjanger H 
+		ON H.MediaID = M.MediaID
+	INNER JOIN Sjanger SJ 
+		ON SJ.SjangerID = H.SjangerID;
