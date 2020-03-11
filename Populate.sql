4140b6e59e1bfbfcd4d7dbf3f27ce1069e824251
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-03-11 14:49:26 +0100

    Russerne kommer

---
+++ Populate.sql
@@ -0,0 +1,43 @@
+# Person
+
+
+# Musikk
+
+
+# Sjanger
+
+
+# Selskap
+
+
+# Bruker
+
+
+# Serie
+
+
+# Sesong
+
+
+# Media
+
+
+# Anmeldelse
+
+
+# SkuespillerI
+
+
+# SkrevetAv
+
+
+# RegiAv
+
+
+# Komponert
+
+
+# MusikkIMedia
+
+
+# Episode i
