2abe9e92588b7a12fee2d566023dfd6d39451b77
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-03-11 15:19:21 +0100

    Fix trailing ; and correct input values

--- Populate.sql
+++ Populate.sql
@@ -66,72 +66,90 @@ VALUES ('3', 'Covid-19');
 
 # Sesong
 INSERT INTO Sesong (Nummer, SerieID)
-VALUES ('1', '1')
+VALUES ('1', '1');
+
 INSERT INTO Sesong (Nummer, SerieID)
-VALUES ('2', '2')
+VALUES ('2', '2');
+
 INSERT INTO Sesong (Nummer, SerieID)
-VALUES ('3', '3')
+VALUES ('3', '3');
 
 # Media
 INSERT INTO Media (MediaID, Tittel, Utgivelsesår, Lanseringsdato, Beskrivelse, Lengde, SelskapID)
-VALUES ('1', 'The Good the Bad and the Zugly', '1998', '1997', 'Kinda long', '3', '1')
+VALUES ('1', 'The Good the Bad and the Zugly', '1998', '1997-01-01', 'Kinda long', '3', '1');
+
 INSERT INTO Media (MediaID, Tittel, Utgivelsesår, Lanseringsdato, Beskrivelse, Lengde, SelskapID)
-VALUES ('2', 'Behemoth', '2000', '1990', 'The movie about the band', '2', '2')
+VALUES ('2', 'Behemoth', '2000', '1990-05-03', 'The movie about the band', '2', '2');
+
 INSERT INTO Media (MediaID, Tittel, Utgivelsesår, Lanseringsdato, Beskrivelse, Lengde, SelskapID)
-VALUES ('3', 'Transformers', '2010', '2009', 'The one about robots', '2', '3')
+VALUES ('3', 'Transformers', '2010', '2009-06-11', 'The one about robots', '2', '3');
 
 # Anmeldelse
-INSERT INTO Anmeldelse (AnmeldelseID, Tekst, Rating, BrukerID, MediaID, Nummer, SerieID)
-VALUES ('1', 'Good movie, I like it.', '10', '1', '1')
-INSERT INTO Anmeldelse (AnmeldelseID, Tekst, Rating, BrukerID, MediaID, Nummer, SerieID)
-VALUES ('2', 'Meeh, seen better', '7', '2', '2')
-INSERT INTO Anmeldelse (AnmeldelseID, Tekst, Rating, BrukerID, MediaID, Nummer, SerieID)
-VALUES ('3', 'This series made me depressed...', '2', '3', '3')
+INSERT INTO Anmeldelse (AnmeldelseID, Tekst, Rating, BrukerID, MediaID)
+VALUES ('1', 'Good movie, I like it.', '10', '1', '1');
+
+INSERT INTO Anmeldelse (AnmeldelseID, Tekst, Rating, BrukerID, MediaID)
+VALUES ('2', 'Meeh, seen better', '7', '2', '2');
+
+INSERT INTO Anmeldelse (AnmeldelseID, Tekst, Rating, BrukerID, MediaID)
+VALUES ('3', 'This series made me depressed...', '2', '3', '3');
 
 # SkuespillerI
 INSERT INTO SkuespillerI (Fnr, MediaID, Rolle)
-VALUES ('1', '1', 'Papa John')
+VALUES ('1', '1', 'Papa John');
+
 INSERT INTO SkuespillerI (Fnr, MediaID, Rolle)
-VALUES ('2', '2', 'Lucky Luke')
+VALUES ('2', '2', 'Lucky Luke');
+
 INSERT INTO SkuespillerI (Fnr, MediaID, Rolle)
-VALUES ('3', '3', 'Luke Smith')
+VALUES ('3', '3', 'Luke Smith');
 
 # SkrevetAv
 INSERT INTO SkrevetAv (Fnr, MediaID)
-VALUES ('1', '1')
+VALUES ('1', '1');
+
 INSERT INTO SkrevetAv (Fnr, MediaID)
-VALUES ('2', '2')
+VALUES ('2', '2');
+
 INSERT INTO SkrevetAv (Fnr, MediaID)
-VALUES ('3', '3')
+VALUES ('3', '3');
 
 # RegiAv
 INSERT INTO RegiAv (Fnr, MediaID)
-VALUES ('1', '1')
+VALUES ('1', '1');
+
 INSERT INTO RegiAv (Fnr, MediaID)
-VALUES ('2', '2')
+VALUES ('2', '2');
+
 INSERT INTO RegiAv (Fnr, MediaID)
-VALUES ('3', '3')
+VALUES ('3', '3');
 
 # Komponert
 INSERT INTO Komponert (Fnr, MusikkID)
-VALUES ('1', '1')
+VALUES ('1', '1');
+
 INSERT INTO Komponert (Fnr, MusikkID)
-VALUES ('2', '2')
+VALUES ('2', '2');
+
 INSERT INTO Komponert (Fnr, MusikkID)
-VALUES ('3', '3')
+VALUES ('3', '3');
 
 # MusikkIMedia
 INSERT INTO MusikkIMedia (MusikkID ,MediaID)
-VALUES ('1', '1')
+VALUES ('1', '1');
+
 INSERT INTO MusikkIMedia (MusikkID ,MediaID)
-VALUES ('2', '2')
+VALUES ('2', '2');
+
 INSERT INTO MusikkIMedia (MusikkID ,MediaID)
-VALUES ('3', '3')
+VALUES ('3', '3');
 
 # EpisodeI
 INSERT INTO EpisodeI (MediaID, Nummer)
-VALUES ('1', '1')
+VALUES ('1', '1');
+
 INSERT INTO EpisodeI (MediaID, Nummer)
-VALUES ('2', '2')
+VALUES ('2', '2');
+
 INSERT INTO EpisodeI (MediaID, Nummer)
-VALUES ('3', '3')
+VALUES ('3', '3');
--- src/main/java/tdt4145/project/Queries.java
+++ src/main/java/tdt4145/project/Queries.java
@@ -1,5 +1,18 @@
 package tdt4145.project;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Queries {
+public class Queries extends DBConn {
+    public void fetchRoles(int fnr, int MediaID) {
+        List<String> roles = new ArrayList<>();
 
+        try {
+            PreparedStatement stmt = connection.prepareStatement("SELECT Rolle FROM Skuespiller WHERE BesetningID  = ? GROUP BY Rolle")
+
+        } catch (SQLException e) {
+            System.out.println(String.format("Error occured during fetchRoles: %s", e.getMessage()));
+        }
+    }
 }
